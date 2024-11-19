74a3c621192863e3c85455af84c82e330f1e4f6c
point mode <125463819+pointmode@users.noreply.github.com>
2024-11-19 20:18:59 +0300

    Update Prog13.kt

--- src/Prog13.kt
+++ src/Prog13.kt
@@ -3,13 +3,13 @@ package KotlinBase
 fun main()
 {
     println("введите число для обратного отсчета :D")
-    val number = readln().toInt()
-    if (number <= 0)
+    val num = readln().toInt()
+    if (num <= 0)
     {
         println("значение должно быть положительным ;(")
         return
     }
-    for (i in number downTo  1)
+    for (i in num downTo  1)
     {
         println("обратный отсчет ;Р $i")
     }
