b7746d7c6c3001bf4e6c25d2d02fff1fb6990dcf
point mode <125463819+pointmode@users.noreply.github.com>
2024-11-19 20:25:35 +0300

    Update Prog5.kt

--- src/Prog5.kt
+++ src/Prog5.kt
@@ -2,11 +2,11 @@ package KotlinBase
 
 fun main()
 {
-    val lines = mutableListOf<String>() // пустой список для хранения строк
+    val list = mutableListOf<String>() // пустой список для хранения строк
     for (i in 1..5) {
         print("введите $i строку: ") // цикл для ввода строк
-        lines.add(readln())
+        list.add(readln())
     }
-    val union = lines.joinToString(separator = "") //ф-ция для объединения строк, separator задает разделитель между эл-ми
+    val union = list.joinToString(separator = "") //ф-ция для объединения строк, separator задает разделитель между эл-ми
     println("объединенные строки: $union")
 }
