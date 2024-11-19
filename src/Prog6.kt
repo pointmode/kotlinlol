847f249da82db137ee8463082d93a3c455633330
point mode <125463819+pointmode@users.noreply.github.com>
2024-11-19 20:26:17 +0300

    Update Prog6.kt

--- src/Prog6.kt
+++ src/Prog6.kt
@@ -6,10 +6,10 @@ fun main()
     val word = readln()
     if (word.length >= 2) //проверка на кол-во символов
     {
-        val first = word.substring(0, 4) // ф-ия, которая определяет индексы извлекаемых символов
+        val fst = word.substring(0, 4) // ф-ия, которая определяет индексы извлекаемых символов
         val last = word.substring(4) // извлекаем оставшиеся символы входной строки
-        val result = "$last $first" // объединение строк
-        println("перевернутая строка: $result")
+        val res = "$last $fst" // объединение строк
+        println("перевернутая строка: $res")
     }
     else
     {
